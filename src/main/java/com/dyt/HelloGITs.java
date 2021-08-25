9803e46fe8276f8b64ee866a9a35b2fd55daac02
lexed <1725394171@qq.com>
2021-08-25 11:19:54 +0800

    git的修改

--- src/main/java/com/dyt/HelloGITs.java
+++ src/main/java/com/dyt/HelloGITs.java
@@ -5,6 +5,9 @@ public class HelloGITs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("lalala");
+		System.out.println("小可爱");
+		System.out.println("小猪猪");
+		System.out.println("gulugulugulu");
 
 	}
 
